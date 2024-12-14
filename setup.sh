8f9c7a2099471138c5f2f5c94c5eef36a572d0aa
Florian Tigoulet <flotig77@gmail.com>
2024-12-14 12:30:03 +0100

    feat(hf)

--- setup.sh
+++ setup.sh
@@ -43,11 +43,7 @@ echo ===== Discord Bot Setup
 echo DISCORD_TOKEN=$1 > ./discord_bot/.env
 HUGGINGFACE_TOKEN="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"
 
-# Log in to Hugging Face using the token
-echo "$HUGGINGFACE_TOKEN" | huggingface-cli login --token
-
 # =============================================
-export USE_NNPACK=0
 
 echo ===== Launch services
 
