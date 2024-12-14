536b1dac8baf598a806877d592473f305551e264
Florian Tigoulet <flotig77@gmail.com>
2024-12-14 13:23:34 +0100

    feat(hf): removed hf and uses sdturbo

--- setup.sh
+++ setup.sh
@@ -42,10 +42,6 @@ echo ===== Discord Bot Setup
 
 echo DISCORD_TOKEN=$1 > ./discord_bot/.env
 
-echo ===== HuggingFace Token
-
-export HUGGINGFACE_TOKEN=$2
-
 # =============================================
 
 echo ===== Launch services
